b5565c61237a80427c74b928fbf9581bea75fcd2
Mohd. Salahudeen <hudeensala948@gmail.com>
2023-08-19 01:25:33 +0530

    anime

--- foodlab/src/components/Testimonial.js
+++ foodlab/src/components/Testimonial.js
@@ -8,11 +8,14 @@ function Testimonial() {
         <div className="path2 w-1 h-8 bg-[#519259]"></div>
         <h1 className="title text-3xl">Testimonial</h1>
       </div>
-      <div className="cr flex ">
-        <Card img="./images/anime.jpg" name="John Doe" />
-        <Card />
-        <Card />
-        <Card />
+      <div className="cr ">
+        <Card img="./images/anime.jpg" name="Ayanokoji" />
+        <Card img="./anime/gojo.jpg" name="Gojo Satoru" />
+        <Card img="./anime/luffy.jpg" name="Monkey D.Luffy" />
+        <Card img="./anime/eren.jpeg" name="Eren Jaeger" />
+        <Card img="./anime/zoro.jpeg" name="Roronoa Zoro" />
+        <Card img="./anime/sanji.jpeg" name="Vinsmoke Sanji" />
+        <Card img="./anime/thorfinn.jpg" name="Thorfinn" />
       </div>
     </div>
   );
