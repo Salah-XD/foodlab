fdc00c3e189d3cc10fdfc322a46f3c028c04f535
Mohd. Salahudeen <hudeensala948@gmail.com>
2023-08-19 23:41:31 +0530

    fork90

--- foodlab/src/components/Fork.js
+++ foodlab/src/components/Fork.js
@@ -2,7 +2,60 @@ import React from "react";
 
 function Fork() {
   return (
-    <div className="ftf px-[90px]">
+    <div className="ftf px-[110px] relative mt-[60px]">
+      {/* line */}
+      <img
+        src="./images/path 1382.svg"
+        alt="path"
+        className="rotate-90 absolute top-[-200px] right-[27%] h-[350px]"
+      />
+      <img
+        src="./images/path 1382.svg"
+        alt="path"
+        className=" absolute top-[125px] right-[2%] h-[350px]"
+      />
+      {/* checker */}
+      <img
+        src="./images/Group 171.svg"
+        alt="checker"
+        className="absolute w-[115px] left-[90%] mt-[-30px]"
+      />
+      <img
+        src="./images/Group 165.svg"
+        alt="checker"
+        className="absolute w-[115px] top-[253px] left-12 -z-10"
+      />
+      <img
+        src="./images/Group 165.svg"
+        alt="checker"
+        className="absolute w-[115px] top-[703px] left-12 -z-10"
+      />
+      <img
+        src="./images/Group 165.svg"
+        alt="checker"
+        className="absolute w-[115px] left-[86%] top-[310px] -z-10"
+      />
+      <img
+        src="./images/Group 165.svg"
+        alt="checker"
+        className="absolute w-[115px] left-[86%] top-[790px] -z-10"
+      />
+      {/* track */}
+      <img
+        src="./images/Group 167.svg"
+        alt="path"
+        className="absolute w-[300px] top-[190px] left-[50%]"
+      />
+      <img
+        src="./images/Group 167.svg"
+        alt="path"
+        className="absolute w-[300px] top-[650px] left-[52%]"
+      />
+      <img
+        src="./images/Group 167.svg"
+        alt="path"
+        className="absolute scale-x-[-1] w-[300px] top-[395px] left-[25%]"
+      />
       <h3 className="text-center text-3xl font-bold mb-3">
         Ensuring safety of food from
       </h3>
@@ -10,7 +63,7 @@ function Fork() {
         Farm <span className="text-[#F0BB62]">to Fork</span>
       </h1>
       <div className="bb">
-        <div className="row1 flex gap-8 items-end mt-8">
+        <div className="row1 flex gap-14 items-end mt-8">
           <img src="./images/px/px.png" alt="farm" className="w-[300px]" />
           <div className="text">
             <h3 className="text-2xl mb-2">
@@ -22,7 +75,7 @@ function Fork() {
             </p>
           </div>
         </div>
-        <div className="row2 flex gap-8 items-end ml-[40%] w-[100%] text-right mt-8">
+        <div className="row2 flex gap-14 items-end ml-[37%] w-[100%] text-right mt-8">
           <div className="text">
             <h3 className="text-2xl mb-2">
               <span className="text-[#519259] font-bold">M</span>anufacturing
@@ -39,7 +92,7 @@ function Fork() {
             className="w-[300px]"
           />
         </div>
-        <div className="row3 flex gap-8 items-end mt-8">
+        <div className="row3 flex gap-14 items-end mt-8">
           <img
             src="./images/Image 27/Image 27.png"
             alt="farm"
